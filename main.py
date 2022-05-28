9538315f6e63685e5f766254f1209ec5e2b56018
outlaw <105610062+0outlaw@users.noreply.github.com>
2022-05-28 00:09:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ while True:
     server_json = requests.get(f"https://games.roblox.com/v1/games/{game_id}/servers/Public?sortOrder=Asc&limit=100&cursor={next_page_cursor}").json()
     print(f"next_page_cursor: {next_page_cursor}")
     try:
+        for server_json_data in server_json["data"]:
+            if "ping" in server_json_data:
+                data.append({"id": server_json_data["id"], "ping": server_json_data["ping"]})
         if server_json["nextPageCursor"] is None:
             break
         else:
             next_page_cursor = server_json["nextPageCursor"]
-            for server_json_data in server_json["data"]:
-                if "ping" in server_json_data:
-                    data.append({"id": server_json_data["id"], "ping": server_json_data["ping"]})
     except KeyError:
         print("nextPageCursor could not be found, exiting program.")
         input("press any key to exit")
