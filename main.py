3d511a8c91696c95bda74f6539f3024edf85a443
outlaw <105610062+0outlaw@users.noreply.github.com>
2022-05-28 00:24:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,22 +14,19 @@ while True:
 while True:
     server_json = requests.get(f"https://games.roblox.com/v1/games/{game_id}/servers/Public?sortOrder=Asc&limit=100&cursor={next_page_cursor}").json()
     print(f"next_page_cursor: {next_page_cursor}")
-    try:
+    if "data" in server_json:
         for server_json_data in server_json["data"]:
             if "ping" in server_json_data:
                 data.append({"id": server_json_data["id"], "ping": server_json_data["ping"]})
+    if "nextPageCursor" in server_json:
         if server_json["nextPageCursor"] is None:
             break
         else:
             next_page_cursor = server_json["nextPageCursor"]
-    except KeyError:
-        print("nextPageCursor could not be found, exiting program.")
-        input("press any key to exit")
-        sys.exit()
 
 try:
     min_ping = min(data, key = lambda x: x["ping"])
     pyperclip.copy(f"Roblox.GameLauncher.joinGameInstance({game_id}, '{min_ping['id']}') // Ping: {min_ping['ping']}")
 except ValueError:
-    print("Roblox returned a [] list, could not find server.")
+    print("could not find server")
     input("press any key to exit")
